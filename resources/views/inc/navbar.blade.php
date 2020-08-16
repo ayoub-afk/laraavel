044a228403ec7ad303feda78752e588cec16814e
unknown <ayoubmessaadi9@gmail.com>
2020-08-16 09:26:06 +0100

    first commit

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="http://127.0.0.1:8000/home1">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    </div>
+</nav>
